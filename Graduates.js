3037e15df7658ab4d91594ff9d9edbfd73890ec3
zazahrah <zahrahsiti29@gmail.com>
2020-01-19 14:41:55 +0700

    upload

---
+++ Graduates.js
@@ -0,0 +1,95 @@
+function graduates (students) {
+  // Code disini
+  var object1 = {}
+  for (var i=0; i<students.length; i++) {
+    var cekClass = students[i].class
+    if (!object1[cekClass]) {
+        var array = []
+        object1[cekClass] = array
+    }
+    if (students[i].score > 75) {
+        var object2 = {}
+        object2.name = students[i].name
+        object2.score = students[i].score
+        object1[cekClass].push(object2)
+    }
+}
+return object1
+}
+
+console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
